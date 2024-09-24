5ce6a5ce3c33cdeb4b5e34c943dac3259e42a7c4
21R01A7263 <21r01a7263@cmritonline.ac.in>
2024-09-24 22:23:36 +0530

    fast sort

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import UserTable from "./UserTable";
 
-const UsersPage = async () => {
+interface Props{
+  searchParams: { sortOrder: string; };
+
+}
+
+const UsersPage = async ({ searchParams: {sortOrder}}: Props) => {
   return (
     <div className="p-5 text-black">
       <h1>Hello World</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <UserTable />
+      <UserTable sortOrder={sortOrder} />
     </div>
   );
 };
